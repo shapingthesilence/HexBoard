c6f6f495a86cc7c786b5048f4a2076a14cfc80f5
Jared DeCook <jared@shapingthesilence.com>
2026-06-15 14:35:50 -0400

    Updated layout names.

--- src/firmware/model/Layout.cpp
+++ src/firmware/model/Layout.cpp
@@ -70,8 +70,8 @@ layoutDef layoutOptions[] = {
   { "Porcupine", 0, 65, 1, -4, TUNING_22EDO },
   { "Full Gamut", 1, 75, 1, -9, TUNING_22EDO },
 
-  { "Janko", 0, 65, 1, -4, TUNING_24EDO },              // Maybe call it "Quartertone Janko"?
-  { "Bosanquet-Wilson", 0, 65, -1, -3, TUNING_24EDO },  // Maybe call it "1/4 tone Bosanquet"?
+  { "Janko", 0, 65, 1, -4, TUNING_24EDO },              // Maybe name it to "Quartertone Janko"?
+  { "Bosanquet-Wilson", 0, 65, -1, -3, TUNING_24EDO },  // Maybe name it to "1/4 tone Bosanquet"?
   { "Full Gamut", 1, 75, 1, -9, TUNING_24EDO },
 
   { "Compressed Janko", 0, 65, -3, -5, TUNING_31EDO },
@@ -79,7 +79,7 @@ layoutDef layoutOptions[] = {
   { "Janko", 0, 65, 2, -5, TUNING_31EDO },
   { "Bosanquet-Wilson", 0, 65, -2, -3, TUNING_31EDO },
   { "Wicki-Hayden", 1, 64, 5, -18, TUNING_31EDO },
-  { "5X -13Y", 1, 64, 5, -13, TUNING_31EDO },  // Unnamed layout, between Wicki-Hayd. and compressed Janko
+  { "5X -13Y", 1, 64, 5, -13, TUNING_31EDO },  // Unnamed layout, between Wicki-Hayden and compressed Janko
   { "Harmonic Table", 0, 75, -18, 8, TUNING_31EDO },
   { "Double Bosanquet", 0, 65, -1, -4, TUNING_31EDO },
   { "Anti-Double Bos.", 0, 65, 1, -5, TUNING_31EDO },
@@ -90,7 +90,7 @@ layoutDef layoutOptions[] = {
   { "Janko", 0, 65, 2, -5, TUNING_31EDO_ZETA },
   { "Bosanquet-Wilson", 0, 65, -2, -3, TUNING_31EDO_ZETA },
   { "Wicki-Hayden", 1, 64, 5, -18, TUNING_31EDO_ZETA },
-  { "5X -13Y", 1, 64, 5, -13, TUNING_31EDO_ZETA },  // Unnamed layout, between Wicki-Hayd. and compressed Janko
+  { "5X -13Y", 1, 64, 5, -13, TUNING_31EDO_ZETA },  // Unnamed layout, between Wicki-Hayden and compressed Janko
   { "Harmonic Table", 0, 75, -18, 8, TUNING_31EDO_ZETA },
   { "Double Bosanquet", 0, 65, -1, -4, TUNING_31EDO_ZETA },
   { "Anti-Double Bos.", 0, 65, 1, -5, TUNING_31EDO_ZETA },
@@ -104,7 +104,7 @@ layoutDef layoutOptions[] = {
   { "Wicki-Hayden", 1, 64, 7, -24, TUNING_41EDO },
   { "Gerhard", 0, 65, 3, -10, TUNING_41EDO },  // forty-one #2
   { "Baldy", 0, 65, -1, -6, TUNING_41EDO },
-  { "Rodan", 1, 65, -1, -7, TUNING_41EDO },
+  { "Rodan A", 1, 65, -1, -7, TUNING_41EDO },
   { "Full Gamut", 1, 75, 1, -9, TUNING_41EDO },  // forty-one #3
 
   { "Janko", 0, 65, 3, -7, TUNING_43EDO },
@@ -125,37 +125,37 @@ layoutDef layoutOptions[] = {
   { "Wicki-Hayden", 1, 64, 9, -31, TUNING_53EDO },
   { "Kleismic A", 0, 65, -8, -3, TUNING_53EDO },
   { "Kleismic B", 0, 65, -5, -3, TUNING_53EDO },
-  { "Buzzard", 0, 65, -9, -1, TUNING_53EDO },
+  { "Buzzard A", 0, 65, -9, -1, TUNING_53EDO },
   { "Compressed Janko", 1, 65, -4, -9, TUNING_53EDO },
   { "Compr. Bosanquet", 1, 65, 4, -13, TUNING_53EDO },
   { "Full Gamut", 1, 75, 1, -9, TUNING_53EDO },
 
-  { "Janko", 0, 64, 3, -10, TUNING_58EDO },           // Maybe call it "Quartertone Janko"?
-  { "Bosanquet-Wilson", 0, 64, 3, 7, TUNING_58EDO },  // Maybe call it "Quartertone Bosanquet"?
-  { "Hemififths", 0, 64, 4, -7, TUNING_58EDO },
-  { "Hemififths Mirror.", 0, 64, -4, -3, TUNING_58EDO },
+  { "Janko", 0, 64, 3, -10, TUNING_58EDO },           // Maybe name it "Quartertone Janko"?
+  { "Bosanquet-Wilson", 0, 64, 3, 7, TUNING_58EDO },  // Maybe name it "Quartertone Bosanquet"?
+  { "Hemififths A", 0, 64, 4, -7, TUNING_58EDO },
+  { "Hemififths B", 0, 64, -4, -3, TUNING_58EDO },
   { "Chromatic", 0, 64, -7, -5, TUNING_58EDO },
   { "Harmonic Table", 0, 75, -34, 15, TUNING_58EDO },
   { "Septimal H.T.", 0, 75, -34, 13, TUNING_58EDO },
   { "Diaschismic", 0, 64, 4, -9, TUNING_58EDO },
-  { "4X -19Y", 0, 64, 4, -19, TUNING_58EDO },    // unnamed layout, efficient for major 7ths, 9s, #11s and so on
-  { "-27X 10Y", 1, 64, -27, 10, TUNING_58EDO },  // weird but efficient layout
+  { "4X -19Y", 0, 64, 4, -19, TUNING_58EDO },    // unnamed layout, good for major 7ths, 9s, #11s and so on
+  { "-27X 10Y", 1, 64, -27, 10, TUNING_58EDO },  // odd but efficient layout
   { "Wicki-Hayd.(29EDO)", 1, 64, 10, -34, TUNING_58EDO },
   { "Bos.Wilson (29EDO)", 0, 65, -6, -4, TUNING_58EDO },
   { "Janko      (29EDO)", 0, 65, 6, -10, TUNING_58EDO },  // 29 EDO subset, each for two rings of fifths
   { "Tridec.H.T.(29EDO)", 0, 75, -34, 14, TUNING_58EDO },
   { "Full Gamut", 1, 75, 1, -9, TUNING_58EDO },
 
-  { "Janko", 0, 64, 3, -10, TUNING_58EDO_ZETA },           // Maybe call it "Quartertone Janko"?
-  { "Bosanquet-Wilson", 0, 64, 3, 7, TUNING_58EDO_ZETA },  // Maybe call it "1/4 tone Bosanquet"?
-  { "Hemififths", 0, 64, 4, -7, TUNING_58EDO_ZETA },
-  { "Hemififths Mirror.", 0, 64, -4, -3, TUNING_58EDO_ZETA },
+  { "Janko", 0, 64, 3, -10, TUNING_58EDO_ZETA },           // Maybe name it "Quartertone Janko"?
+  { "Bosanquet-Wilson", 0, 64, 3, 7, TUNING_58EDO_ZETA },  // Maybe name it "Quartertone Bosanquet"?
+  { "Hemififths A", 0, 64, 4, -7, TUNING_58EDO_ZETA },
+  { "Hemififths B", 0, 64, -4, -3, TUNING_58EDO_ZETA },
   { "Chromatic", 0, 64, -7, -5, TUNING_58EDO_ZETA },
   { "Harmonic Table", 0, 75, -34, 15, TUNING_58EDO_ZETA },
   { "Septimal H.T.", 0, 75, -34, 13, TUNING_58EDO_ZETA },
   { "Diaschismic", 0, 64, 4, -9, TUNING_58EDO_ZETA },
   { "4X -19Y", 0, 64, 4, -19, TUNING_58EDO_ZETA },    // unnamed layout, efficient for major 7ths, 9s, #11s and so on
-  { "-27X 10Y", 1, 64, -27, 10, TUNING_58EDO_ZETA },  // weird but efficient layout
+  { "-27X 10Y", 1, 64, -27, 10, TUNING_58EDO_ZETA },  // odd but efficient layout
   { "Wicki-Hayd.(29EDO)", 1, 64, 10, -34, TUNING_58EDO_ZETA },
   { "Bos.Wilson (29EDO)", 0, 65, -6, -4, TUNING_58EDO_ZETA },
   { "Janko      (29EDO)", 0, 65, 6, -10, TUNING_58EDO_ZETA },  // 29 EDO subset, each for two rings of fifths
@@ -188,8 +188,8 @@ layoutDef layoutOptions[] = {
   { "Wic.Hayd. Pyth.", 1, 64, 15, -51, TUNING_87EDO },    // pythagorean thirds, octave is preserved, note variety decreased
   { "Janko (Good 4/3)", 0, 65, 6, -14, TUNING_87EDO },    // Less efficient but allows perfect chord inversions
   { "Bos.W.(Good 4/3)", 0, 65, -6, -8, TUNING_87EDO },
-  { "Bos.W.(26EDO)", 0, 65, -9, -6, TUNING_87EDO },
-  { "Janko (26EDO)", 0, 65, 9, -15, TUNING_87EDO },
+  { "Bos.W.(29EDO)", 0, 65, -9, -6, TUNING_87EDO },
+  { "Janko (29EDO)", 0, 65, 9, -15, TUNING_87EDO },
   { "Full Gamut", 1, 75, 1, -9, TUNING_87EDO },
 
   { "Standard", 0, 65, -2, -1, TUNING_BP },
